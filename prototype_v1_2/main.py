373a6476a43954378c58fd1b3be39cd6b0275804
Khai Duc <hzpro1221>
2025-09-23 20:48:16 +0700

    update main func

--- prototype_v1_2/main.py
+++ prototype_v1_2/main.py
@@ -1,62 +1,90 @@
-from preprocessor import load_vector_database_from_json
-from audio_text_embedder import get_audio_text_similarity
-from image_text_embedder import get_image_text_similarity
-from video_text_embedder import get_video_text_similarity
-from text_text_embedder import get_text_text_similarity
+from preprocessor import load_video_embeds
+from extent_network import NeuralNetworkRetrieverimport
 
 import os
-import random
-import subprocess
-import time
 
-print("\n🔍 Loading vector database...")
-clusters = load_vector_database_from_json("vector_database.json")
-cluster_embeddings = [cluster["cluster_name"] for cluster in clusters]
+import torch
+
+model = NeuralNetworkRetriever().to("cuda" if torch.cuda.is_available() else "cpu")
+model.load_state_dict(torch.load("/content/model.pth"))
+
+VIDEO_EMBEDS_PATH = "video_embeds.json"
+GET_VIDEO_EMBEDDER = model._video_combine_embeds
+GET_TEXT_EMBEDDER =  model._text_combine_embeds
+
+print("\nLoading video embeds...")
+video_embeds = load_video_embeds(VIDEO_EMBEDS_PATH, device="gpu")
 
 def content_retrieve(query):
-    # Step 1: Predict cluster
-    max_score = -float("inf")
-    predicted_cluster = None
-
-    for cluster in clusters:
-        score = get_text_text_similarity(
-            topic_embed=cluster["cluster_name"]["vector"],
-            query=query
-        )
-        if score > max_score:
-            max_score = score
-            predicted_cluster = cluster
-
-    # Step 2: Predict video in cluster
-    video_scores = []                
-    for video in predicted_cluster["videos"]:
-        total_score = 0
-        for model_name, audio_embedding in video.get("audio_embedding", {}).items():
-            total_score += get_audio_text_similarity(audio_embedding, query, model=model_name)
-
-        for model_name, video_embedding in video.get("video_embedding", {}).items():
-            total_score += get_video_text_similarity(video_embedding, query, model=model_name)
-
-        for model_name, frame_embeddings in video.get("keyframe_embedding", {}).items():
-            frame_scores = [
-                get_image_text_similarity(frame_embed, query, model=model_name)
-                for frame_embed in frame_embeddings
-            ]
-            if frame_scores:
-                total_score += sum(frame_scores) / len(frame_scores)
-
-        video_scores.append((video["video_path"], total_score))        
+    print("Getting video embed...\n")
+    candidate_vids = torch.cat([video_embed.unsqueeze(0) for video_path, video_embed in video_embeds.items()])
+    video_embeds_output = model.get_video_embed(candidate_vids.unsqueeze(0))
     
-    queried_video_path = video_scores[0][0]
-    queried_video_score = video_scores[0][1]
+    print("Getting text embed...\n")
+    text_embed_output = model.get_text_embed(GET_TEXT_EMBEDDER([query]))
 
-    return queried_video_path, queried_video_score 
+    sim = model.compute_similarity(text_embed_output, video_embeds_output)
+    topk_values, topk_indices = sim.topk(3, dim=-1)  # along candidate dim
 
-if __name__ == "__main__":
-    start_time = time.perf_counter()
+    keys = list(video_embeds.keys())
+    for i, (score, idx) in enumerate(zip(topk_values[0], topk_indices[0])):
+        video_path = keys[idx]
+        filename = os.path.basename(video_path)
+        print(f"Video: {filename}, Similarity Score: {score.item():.4f}")
     
-    video_path, video_score = content_retrieve("Today, I will talk about Machine learning")
-    print("out_video: ", video_path, "; video_score: ", video_score) 
+    best_video_path = keys[topk_indices[0][0]]
+    best_score = topk_values[0][0].item()
+    
+    return best_video_path, best_score
 
-    end_time = time.perf_counter()
-    print(f"Execution time: {end_time - start_time:.4f} seconds")    
+
+# clusters = load_vector_database_from_json("video_embeds.json")
+# cluster_embeddings = [cluster["cluster_name"] for cluster in clusters]
+
+# def content_retrieve(query):
+#     # Step 1: Predict cluster
+#     max_score = -float("inf")
+#     predicted_cluster = None
+
+#     for cluster in clusters:
+#         score = get_text_text_similarity(
+#             topic_embed=cluster["cluster_name"]["vector"],
+#             query=query
+#         )
+#         if score > max_score:
+#             max_score = score
+#             predicted_cluster = cluster
+
+#     # Step 2: Predict video in cluster
+#     video_scores = []                
+#     for video in predicted_cluster["videos"]:
+#         total_score = 0
+#         for model_name, audio_embedding in video.get("audio_embedding", {}).items():
+#             total_score += get_audio_text_similarity(audio_embedding, query, model=model_name)
+
+#         for model_name, video_embedding in video.get("video_embedding", {}).items():
+#             total_score += get_video_text_similarity(video_embedding, query, model=model_name)
+
+#         for model_name, frame_embeddings in video.get("keyframe_embedding", {}).items():
+#             frame_scores = [
+#                 get_image_text_similarity(frame_embed, query, model=model_name)
+#                 for frame_embed in frame_embeddings
+#             ]
+#             if frame_scores:
+#                 total_score += sum(frame_scores) / len(frame_scores)
+
+#         video_scores.append((video["video_path"], total_score))        
+    
+#     queried_video_path = video_scores[0][0]
+#     queried_video_score = video_scores[0][1]
+
+#     return queried_video_path, queried_video_score 
+
+# if __name__ == "__main__":
+#     start_time = time.perf_counter()
+    
+#     video_path, video_score = content_retrieve("Today, I will talk about Machine learning")
+#     print("out_video: ", video_path, "; video_score: ", video_score) 
+
+#     end_time = time.perf_counter()
+#     print(f"Execution time: {end_time - start_time:.4f} seconds")    
--- prototype_v1_2/preprocessor.py
+++ prototype_v1_2/preprocessor.py
@@ -23,11 +23,12 @@ if __name__ == "__main__":
     model = NeuralNetworkRetriever().to("cuda" if torch.cuda.is_available() else "cpu")
     model.load_state_dict(torch.load("/content/model.pth"))
 
-    EVALUATE_FOLDER = "evaluate_videos"
+    EVALUATE_FOLDER = "/content/Multi-modal-audio-visual-co-analysis-to-identify-complex-contexts-and-define-respective-responses/prototype_v1_2/evaluate_videos"
     GET_VIDEO_EMBEDDER = model._video_combine_embeds
     GET_TEXT_EMBEDDER =  model._text_combine_embeds
 
-    video_paths = [p.relative_to(EVALUATE_FOLDER) for p in Path(EVALUATE_FOLDER).rglob("*.mp4")]
+    video_paths = list(Path(EVALUATE_FOLDER).rglob("*.mp4"))
+    print(video_paths)
     video_embeds = {}
     print("preprocess video...")
 
@@ -39,80 +40,3 @@ if __name__ == "__main__":
 
     print("save video embeds...")
     save_video_embeds(video_embeds, "video_embeds.json")
-
-    # audio_output_folder = "tmp/audio"
-    # keyframe_output_folder = "tmp/keyframe"
-
-    # # Remove folder and its contents, then recreate empty folder
-    # shutil.rmtree(audio_output_folder)
-    # os.makedirs(audio_output_folder)
-
-    # shutil.rmtree(keyframe_output_folder)
-    # os.makedirs(keyframe_output_folder)
-
-    # # Load test video
-    # with open('testset_metadata.json', 'r', encoding='utf-8') as f:
-    #     clusters = json.load(f)
-    
-    # for cluster in clusters:
-    #     # Embedd cluster_name into vector 
-    #     cluster_name = cluster["cluster_name"]
-    #     cluser_name_vector = get_text_embedding(cluster_name)
-
-    #     del cluster["cluster_name"]
-    #     cluster["cluster_name"] = {
-    #         "name": cluster_name,
-    #         "vector": cluser_name_vector  
-    #     }
-        
-    #     for video in cluster["videos"]:
-    #         video["audio_embedding"] = {}
-    #         video["video_embedding"] = {}
-    #         video["keyframe_embedding"] = {}
-
-    #         audio_path = extract_audio_from_video(
-    #             video_path=video["video_path"],
-    #             output_path=audio_output_folder
-    #         )
-
-    #         for model in audio_embed_model:
-    #             audio_embed = get_audio_embeddings(audio_path=audio_path, model=model)
-    #             video["audio_embedding"][model] = audio_embed
-         
-    #         print("Loading video:", video["video_path"])
-    #         print("Exists:", os.path.exists(video["video_path"]))
-    #         print("Extension:", os.path.splitext(video["video_path"])[1])
-    #         for model in video_embed_model:
-    #             video_embed = get_video_embedding(video_path=video["video_path"], model=model)
-    #             video["video_embedding"][model] = video_embed 
-
-    #         keyframe_paths = extract_keyframe_from_video(
-    #             video_path=video["video_path"],
-    #             num_frame=16,
-    #             output_folder=keyframe_output_folder
-    #         )
-
-    #         for model in image_embed_model:
-    #             video["keyframe_embedding"][model] = []
-    #             for keyframe_path in keyframe_paths:
-    #                 img_embed = get_image_embedding(image_path=keyframe_path, model=model)
-    #                 video["keyframe_embedding"][model].append(img_embed)
-            
-    #         chunk_embedds = []
-    #         # Embedding text chunk
-    #         for chunk_text in video["chunks"]:
-    #             chunk_embedd = get_text_embedding(chunk_text)
-    #             chunk_embedds.append(
-    #                 {
-    #                     "text": chunk_text,
-    #                     "vector": chunk_embedd 
-    #                 }
-    #             )
-    #         del video["chunks"]
-    #         video["chunks"] = chunk_embedds 
-
-    save_vector_database_to_json(
-        vector_database=clusters,
-        output_path="vector_database.json"
-    )
-
